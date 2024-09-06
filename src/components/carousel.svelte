c93b49fc4bf03237b60048b8a0667e48bb81500e
Murkuin <hudsonjohnson.visualmedia@gmail.com>
2024-09-06 15:25:20 +0100

    feat: Update home carousel component
    This commit updates the home carousel component to use a new Carousel component, which provides improved functionality and styling for the carousel. It also adds a new slide to the carousel, showcasing the dedication of the Orkney Islands Council to their islands. This change enhances the user experience by making the carousel more interactive and visually appealing.

---
+++ src/components/carousel.svelte
@@ -0,0 +1,61 @@
+<script>
+  import { onDestroy, onMount } from 'svelte';
+  let currentSlide = 0;
+  let slides = [];
+  // let interval;
+
+  onMount(() => {
+    slides = document.querySelectorAll('.carousel-item');
+    // interval = setInterval(nextSlide, 7000)
+  });
+
+  // onDestroy(() => {
+  //   clearInterval(interval);
+  // });
+
+  function nextSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+    smoothScrollTo(slides[currentSlide]);
+  }
+
+  function prevSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    slides[currentSlide].classList.add('active');
+    smoothScrollTo(slides[currentSlide]);
+  }
+
+  function smoothScrollTo(element) {
+    element.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
+  }
+</script>
+
+<style>
+  .carousel-item {
+    transition: transform 0.5s ease-in-out;
+  }
+  .carousel-item.active {
+    transform: translateX(0);
+  }
+  .carousel-item:not(.active) {
+    transform: translateX(100%);
+  }
+</style>
+
+<div class="carousel relative w-full overflow-hidden h-full">
+  <slot />
+  <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+    <button on:click={prevSlide} class="text-4xl rotate-180 ml-8" aria-label="carousel back">
+      <svg width="14" height="26" viewBox="0 0 14 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1.375 1.75L12.625 13L1.375 24.25" stroke="#1E1E1E" stroke-width="2.25" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+    </button>
+    <button on:click={nextSlide} class="text-4xl mr-8" aria-label="carousel forward">
+      <svg width="14" height="26" viewBox="0 0 14 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1.375 1.75L12.625 13L1.375 24.25" stroke="#1E1E1E" stroke-width="2.25" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+    </button>
+  </div>
+</div>

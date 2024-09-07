766ca92653b429435542dc4ff7a934b427dc111f
Murkuin <hudsonjohnson.visualmedia@gmail.com>
2024-09-07 08:25:08 +0100

    chore: Update astro.config.mjs with site and base configuration
    This commit updates the astro.config.mjs file to include the site and base configuration. The site configuration is set to 'https://hj-vm.github.io' and the base configuration is set to '/recruitment'. This change ensures that the website is correctly deployed to the specified site and base URL.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,6 @@ import svelte from '@astrojs/svelte';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
+  site: 'https://hj-vm.github.io',
+  base: '/recruitment',
 });

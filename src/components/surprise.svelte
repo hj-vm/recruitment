e602179b5b67711b2cd6c7fb566ac47decba2e2c
Murkuin <hudsonjohnson.visualmedia@gmail.com>
2024-09-07 09:11:43 +0100

    refactor: Update header links to use '/recruitment' prefix

---
+++ src/components/surprise.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { onMount } from 'svelte';
+    let randomLink = '';
+
+    async function fetchRssFeed(url) {
+        const response = await fetch(url);
+        const text = await response.text();
+        const parser = new DOMParser();
+        const xml = parser.parseFromString(text, 'application/xml');
+        const items = Array.from(xml.querySelectorAll('item'));
+        const randomItem = items[Math.floor(Math.random() * items.length)];
+        return randomItem.querySelector('link').textContent;
+    }
+
+    onMount(async () => {
+        const rssUrl = 'https://myjobscotland.gov.uk/api/v1/rss?alias=councils/orkney-islands-council/jobs/rss';
+        randomLink = await fetchRssFeed(rssUrl);
+    });
+</script>
+
+<button class="group mb-4 w-fit px-4 pb-1 rounded-full text-xl border-white border-solid border transition-all hover:transition-all hover:shadow-lg"><a class="text-white font-light transition-all group-hover:font-normal group-hover:drop-shadow-lg" href={randomLink} target="_blank">surprise me</a></button>
